493392bc8bca263347a273962a98453629328a7c
Filip Krężel <filip.0krezel@gmail.com>
2025-10-28 17:44:07 +0100

    change branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://flixirr.github.io",
+  base: "portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
