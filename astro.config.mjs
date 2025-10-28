38ce31bdab5962a62dfd652aa18b6db31140b1ce
Filip Krężel <filip.0krezel@gmail.com>
2025-10-28 17:15:11 +0100

    add pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://flixirr.github.io",
+  base: "/flixirr.github.io/",
   vite: {
     plugins: [tailwindcss()],
   },
